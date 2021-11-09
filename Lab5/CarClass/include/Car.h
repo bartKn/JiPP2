bb331b80d75de9cc59a9a0a77f16120307b2fbc6
Bartosz Kniaziewicz <605bart@gmail.com>
2021-11-09 10:01:42 +0100

    Lab5 - car class

---
+++ Lab5/CarClass/include/Car.h
@@ -0,0 +1,17 @@
+#ifndef JIPPV2_CAR_H
+#define JIPPV2_CAR_H
+
+#include <iostream>
+
+using namespace std;
+
+class Car {
+private:
+    string idNumber;
+    string name;
+    int seats;
+
+};
+
+
+#endif //JIPPV2_CAR_H
